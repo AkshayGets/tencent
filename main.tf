d3140ded4ec377b7709c49b92fd8e1ef0324a77f
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2026-04-27 15:00:18 +0530

    Remove tencentcloud_instance and add availability zones
    Removed simple_vm instance resource and added data source for availability zones.

--- main.tf
+++ main.tf
@@ -11,13 +11,8 @@ provider "tencentcloud" {
   region = "ap-guangzhou"
 }
 
-resource "tencentcloud_instance" "simple_vm" {
-  instance_name     = "simple-vm"
-  availability_zone = "ap-guangzhou-3"
-  image_id          = "img-9qabwvbn"  # Ubuntu 20.04 LTS image ID (example, verify in your account)
-  instance_type     = "S1.SMALL1"
-  system_disk_type  = "CLOUD_PREMIUM"
-  system_disk_size  = 50
-  allocate_public_ip = true
-  internet_max_bandwidth_out = 10
+data "tencentcloud_availability_zones" "zones" {}
+
+output "zones" {
+  value = data.tencentcloud_availability_zones.zones.zones
 }

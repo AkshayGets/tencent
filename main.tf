a33e2e0582afe7e0dfb1a20cb19e9b919c330dc2
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2026-04-27 06:38:22 +0000

    first

--- main.tf
+++ main.tf
@@ -8,5 +8,16 @@ terraform {
 }
 
 provider "tencentcloud" {
-  # Configuration options
+  region = "ap-guangzhou"
+}
+
+resource "tencentcloud_instance" "simple_vm" {
+  instance_name     = "simple-vm"
+  availability_zone = "ap-guangzhou-3"
+  image_id          = "img-9qabwvbn"  # Ubuntu 20.04 LTS image ID (example, verify in your account)
+  instance_type     = "S1.SMALL1"
+  system_disk_type  = "CLOUD_PREMIUM"
+  system_disk_size  = 50
+  allocate_public_ip = true
+  internet_max_bandwidth_out = 10
 }

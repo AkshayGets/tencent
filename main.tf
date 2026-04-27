9a9931653af87a9ede7ac68b43a6f458e2f2a19c
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2026-04-27 06:30:46 +0000

    main file

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    tencentcloud = {
+      source  = "tencentcloudstack/tencentcloud"
+      version = "1.82.89"
+    }
+  }
+}
+
+provider "tencentcloud" {
+  # Configuration options
+}
